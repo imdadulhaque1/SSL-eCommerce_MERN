5c99b2976a704a6d5b485887d19a94af570b4208
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-07-14 11:31:42 +0600

    Server Successfully connected with MongoDB

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv/config');
+const app=require('./app');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL_LOCAL)
+    .then(()=>console.log("Connected to MongoDB!"))
+    .catch(err =>console.error("MongoDB Connection Failed!"));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () =>{
+    console.log(`Server Running on PORT @${port}!`);
+})
