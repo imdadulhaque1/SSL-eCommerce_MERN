685da984e7ddfcc40f59a4da3579ec30ec354d4c
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-07-18 19:33:02 +0600

    App Code structured to cut in index.js file in middleware

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,10 @@
 require('express-async-errors');
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
 const error = require('./middlewares/error');
 
-const userRoute = require('./api/routers/userRoute');
-const categoryRoute = require('./api/routers/categoryRoute');
-const productRoute = require('./api/routers/productRoute');
-
-
-app.use(express.json());
-app.use(cors());
-
-if(process.env.NODE_ENV === "development"){
-    app.use(morgan('dev'));
-}
-
-app.use('/api/user', userRoute);
-app.use('/api/category', categoryRoute);
-app.use('/api/product', productRoute);
-
+require('./middlewares')(app);
+require('./middlewares/routes')(app);
 app.use(error);
 
 module.exports = app;
