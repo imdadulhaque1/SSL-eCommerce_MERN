d9e8f2138c76294abfce22880e6bdbccc836aa0f
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-07-18 19:36:12 +0600

    Done Coding Structured!

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const error = require('./middlewares/error');
 
+// Coding Structured and linked with index.js
 require('./middlewares')(app);
 require('./middlewares/routes')(app);
 app.use(error);
