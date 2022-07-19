0fb8781eda929a25b16117db0d74be342bdf43bf
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-07-19 11:01:26 +0600

    cart route done and cart controller structured!!!

---
+++ backend/api/controllers/cartController.js
@@ -0,0 +1,21 @@
+const _=require("lodash");
+const {CartItemModel} = require("../models/CartItemModel");
+
+const createCartItem = async(req, res) =>{
+
+}
+const getCartItem = async(req, res) =>{
+    
+}
+const updateCartItem = async(req, res) =>{
+    
+}
+
+const deleteCartItem = async(req, res) =>{
+    
+}
+
+module.exports={
+    createCartItem, getCartItem,
+    updateCartItem, deleteCartItem
+}
--- backend/middlewares/routes.js
+++ backend/middlewares/routes.js
@@ -1,10 +1,11 @@
 const userRoute = require('../api/routers/userRoute');
 const categoryRoute = require('../api/routers/categoryRoute');
 const productRoute = require('../api/routers/productRoute');
-
+const cartRouter = require("../api/routers/cartRoute");
 
 module.exports = (app) => {
     app.use('/api/user', userRoute);
     app.use('/api/category', categoryRoute);
     app.use('/api/product', productRoute);
+    app.use('/api/cart', cartRouter);
 }
